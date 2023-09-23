04640eb54ea5bf6476cf0253ec984dbe70ea9c4f
clara <jha235@sfu.ca>
2023-09-22 23:33:36 -0700

    Folder structure and config
    Created project structure and added config file in gitignore

---
+++ src/reddit_search.py
@@ -0,0 +1,16 @@
+import requests, os, time
+import pandas as pd
+import sys
+import os, sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+from config.config import reddit_api_key
+
+api_url = 'https://www.reddit.com/'
+headers = {
+
+}
+params = { 
+
+}
