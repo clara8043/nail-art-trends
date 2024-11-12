974f3c60fb9348b96712ceafb4c9f2b2fc9354d7
clara <jha235@sfu.ca>
2024-11-11 17:45:10 -0800

    Restructured config
    restructured config

--- scripts/reddit_daily.py
+++ scripts/reddit_daily.py
@@ -5,25 +5,37 @@ import praw
 from datetime import date
 from datetime import timedelta 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-#import config.config as reddit
+from config.config import RedditConfig
 
-reddit = praw.Reddit(
-    client_id = os.environ['CLIENT_ID'],
-    client_secret = os.environ['CLIENT_SECRET'],
-    password = os.environ['PASSWORD'],
-    username = os.environ['USERNAME'],
-    user_agent = os.environ['USER_AGENT']
-)
+def connect_reddit() : 
+    if(os.getenv("GITHUB_ACTIONS") != False) :
+        reddit_config = RedditConfig()
+        reddit = praw.Reddit(
+            client_id = reddit_config.CLIENT_ID,
+            client_secret = reddit_config.CLIENT_SECRET,
+            password = reddit_config.PASSWORD,
+            username = reddit_config.USERNAME,
+            user_agent = reddit_config.USER_AGENT
+        )
+    else :
+        reddit = praw.Reddit(
+        client_id = os.environ['CLIENT_ID'],
+        client_secret = os.environ['CLIENT_SECRET'],
+        password = os.environ['PASSWORD'],
+        username = os.environ['USERNAME'],
+        user_agent = os.environ['USER_AGENT']
+        )
+    return reddit    
 
-def get_new_submission(subreddits):
+def get_new_submission(subreddits, reddit):
     all_submission = []
     for subreddit in subreddits:
         for submission in reddit.subreddit(subreddit).new():
             all_submission.append(submission)
     return all_submission
 
-
 def main():
+    reddit = connect_reddit()
     week = date.today() - timedelta(date.today().weekday()) + timedelta(6)
     file_name = sys.path[0]+ "/../data/collected_data/" + week.strftime('%Y_%m_%d') + ".xlsx"
     subreddits = ['Nails', 'RedditLaqueristas', 'NailArt']
@@ -37,7 +49,7 @@ def main():
         all_posts_df = pd.DataFrame(columns=['id','title','image_url'])
 
     # Get today's new posts
-    new_posts_df = pd.DataFrame(pd.Series(get_new_submission(subreddits)).unique(),columns=['id'])
+    new_posts_df = pd.DataFrame(pd.Series(get_new_submission(subreddits, reddit)).unique(),columns=['id'])
     new_posts_df['title'] = new_posts_df['id'].apply(lambda x : x.title if str(x) not in set(all_posts_df['id']) else '0')
     new_posts_df = new_posts_df[new_posts_df['title']!='0']
     new_posts_df['image_url'] = new_posts_df['id'].apply(lambda x : x.url)
--- scripts/reddit_weekly.py
+++ scripts/reddit_weekly.py
@@ -5,31 +5,43 @@ import praw
 from datetime import date
 from datetime import timedelta 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-#import config.config as reddit
+from config.config import reddit
 
-reddit = praw.Reddit(
-    client_id = os.environ['CLIENT_ID'],
-    client_secret = os.environ['CLIENT_SECRET'],
-    password = os.environ['PASSWORD'],
-    username = os.environ['USERNAME'],
-    user_agent = os.environ['USER_AGENT']
-)
+def connect_reddit() : 
+    if(os.getenv("GITHUB_ACTIONS") != False) :
+        reddit_config = RedditConfig()
+        reddit = praw.Reddit(
+            client_id = reddit_config.CLIENT_ID,
+            client_secret = reddit_config.CLIENT_SECRET,
+            password = reddit_config.PASSWORD,
+            username = reddit_config.USERNAME,
+            user_agent = reddit_config.USER_AGENT
+        )
+    else :
+        reddit = praw.Reddit(
+        client_id = os.environ['CLIENT_ID'],
+        client_secret = os.environ['CLIENT_SECRET'],
+        password = os.environ['PASSWORD'],
+        username = os.environ['USERNAME'],
+        user_agent = os.environ['USER_AGENT']
+        )
+    return reddit    
 
-def get_top_submission(subreddits):
+def get_top_submission(subreddits, reddit):
     all_submission = []
     for subreddit in subreddits:
         for submission in reddit.subreddit(subreddit).top(time_filter="week"):
             all_submission.append(submission)
     return all_submission
 
-
 def main():
+    reddit = connect_reddit()
     week = date.today() - timedelta(date.today().weekday()) + timedelta(6)
     file_name = sys.path[0]+ "/../data/collected_data/" + week.strftime('%Y_%m_%d') + ".xlsx"
     subreddits = ['Nails', 'RedditLaqueristas', 'NailArt']
 
     # Get this week's top posts
-    top_posts_df = pd.DataFrame(pd.Series(get_top_submission(subreddits)).unique(),columns=['id'])
+    top_posts_df = pd.DataFrame(pd.Series(get_top_submission(subreddits, reddit)).unique(),columns=['id'])
     top_posts_df['title'] = top_posts_df['id'].apply(lambda x : x.title if str(x) not in set(top_posts_df['id']) else '0')
     top_posts_df = top_posts_df[top_posts_df['title']!='0']
     top_posts_df['image_url'] = top_posts_df['id'].apply(lambda x : x.url)
